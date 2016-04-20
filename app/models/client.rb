fab1d4dc44ddba0dbcbe5e0b4716a8d7de2a1e5a
Dakota Martinez <dakotaleemusic@gmail.com>
2016-04-20 01:51:13 +0000

    adds client associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ActiveRecord::Base
+  belongs_to :user
+  has_many :appointments 
+  has_many :locations, through: :appointments
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -20,5 +20,34 @@ RSpec.describe Client, type: :model do
     
   end
   
+  context "associations" do 
+    
+    let(:two_days_later) { DateTime.now.midnight + 59.hours }
+    let(:next_week) { DateTime.now.midnight + 14.hours + 7.days }
+    
+    let(:santa_monica) { Location.find_or_create_by(nickname: "Santa Monica Music", city: "Santa Monica", street_address: "1901 Santa Monica Blvd", state: "CA", zipcode: "90404", business_name: "Santa Monica Music Center") }
+    let(:culver_city) { Location.find_or_create_by(nickname: "Culver City Music", city: "Culver City", street_address: "10862 Washington Blvd", state: "CA", zipcode: "90232", business_name: "Culver City Music Center") }
+    
+    let(:appointment1) { Appointment.create( appointment_time: two_days_later, duration: 3600, price: 80, location_id: santa_monica.id, client_id: axel.id, user_id: sandra.id) }
+    let(:appointment2) { Appointment.create( appointment_time: next_week, duration: 5400, price: 110, location_id: culver_city.id, client_id: axel.id, user_id: sandra.id) }
+    
+    it "belongs to a user" do 
+      expect(axel.user).to eq(sandra)
+    end
+
+    it "has many appointments" do
+      [appointment1, appointment2]
+      axel.save
+      expect(axel.appointments).to include(appointment1, appointment2)
+    end
+    
+    it "has many locations through appointments" do 
+      [appointment1, appointment2]
+      axel.save 
+      expect(axel.locations).to include(santa_monica, culver_city)
+    end
+    
+  end
+  
 end
 
